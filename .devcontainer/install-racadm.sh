3f5ec8c74f305de5c44b8fd42a9197fff60a5051
solidDoWant <fred.heinecke@yahoo.com>
2024-10-01 05:58:20 +0000

    Add r730xd bootloader (clover) setup
    Signed-off-by: solidDoWant <fred.heinecke@yahoo.com>

--- .devcontainer/install-racadm.sh
+++ .devcontainer/install-racadm.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
-VERSION="${1}"
+VERSION="11010"
 OS_RELEASE='jammy'  # 'noble' is not supported yet
 
 KEYRING_PATH='/usr/share/keyrings/linux.dell.com.sources-keyring.asc'
 KEYRING_URL=' https://linux.dell.com/repo/pgp_pubkeys/0x1285491434D8786F.asc'
 REPO_URL="http://linux.dell.com/repo/community/openmanage/${VERSION}/${OS_RELEASE}"
+SOURCES_FILE="/etc/apt/sources.list.d/linux.dell.com.sources.list"
 
 sudo curl -fsSL -o "${KEYRING_PATH}" "${KEYRING_URL}"
 
 echo "deb [signed-by=${KEYRING_PATH}] ${REPO_URL} ${OS_RELEASE} main" \
-    | sudo tee -a /etc/apt/sources.list.d/linux.dell.com.sources.list
+    | sudo tee -a "${SOURCES_FILE}"
 
 sudo apt install -y srvadmin-hapi || sudo dpkg --configure -a
 sudo apt install -y srvadmin-idracadm8 || sudo dpkg --configure -a
+
+# Something later in the build process is dumping the keyfiles so the source
+# must be removed, or later apt invocations will fail
+sudo rm -vf "${SOURCES_FILE}"
