4ff56cb49d2fd55c46ded0fa24504217105ef554
solidDoWant <fred.heinecke@yahoo.com>
2025-09-12 03:28:10 +0000

    first pass port forward to VPN network
    Signed-off-by: solidDoWant <fred.heinecke@yahoo.com>

--- cluster/gitops/networking/vpn/gateway/scripts/setup-nat.sh
+++ cluster/gitops/networking/vpn/gateway/scripts/setup-nat.sh
@@ -8,18 +8,27 @@ set -eu
 
 set -x
 
+PROTOCOLS=${PROTOCOLS:-"tcp udp"}
+
 # Rewrite packets that are port-forwarded by the VPN to the destination IP:Port combo
 for PORT in ${PORT_FORWARDING_PORTS}; do
+    DESTINATION_IP="${PORT_FORWARD_DESTINATION_IP_PREFIX}.${LAST_OCTET}"
+
     for PROTOCOL in ${PROTOCOLS}; do
-        iptables -t nat -A PREROUTING -i "${VPN_INTERFACE}" -p "${PROTOCOL}" --dst "${WIREGUARD_ADDRESSES}" --dport "${PORT}" -j DNAT --to-destination "${PORT_FORWARD_DESTINATION_IP}":"${PORT}"
+        iptables -t nat -A PREROUTING -i "${VPN_INTERFACE}" -p "${PROTOCOL}" --dst "${WIREGUARD_ADDRESSES}" --dport "${PORT}" -j DNAT --to-destination "${DESTINATION_IP}":"${PORT}"
     done
 done
 
 # Rewrite the source IP of outgoing packets to the local interface's IP
+LAST_OCTET=1
 for PORT in ${PORT_FORWARDING_PORTS}; do
+    DESTINATION_IP="${PORT_FORWARD_DESTINATION_IP_PREFIX}.${LAST_OCTET}"
+
     for PROTOCOL in ${PROTOCOLS}; do
-        iptables -t nat -A POSTROUTING -o "${LOCAL_INTERFACE}"  -p "${PROTOCOL}" --dst "${PORT_FORWARD_DESTINATION_IP}" --dport "${PORT}" -j MASQUERADE
+        iptables -t nat -A POSTROUTING -o "${GATEWAY_NETWORK_INTERFACE}"  -p "${PROTOCOL}" --dst "${DESTINATION_IP}" --dport "${PORT}" -j MASQUERADE
     done
+
+    LAST_OCTET=$((LAST_OCTET + 1))
 done
 
 # Rewrite packets that come in the local interface to the VPN interface with the VPN interface source address, but only if they are not destined for the local machine
