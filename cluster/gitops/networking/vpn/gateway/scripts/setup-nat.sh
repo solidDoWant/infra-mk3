9cfc61940c95ba10ea68f2e0a77a2195e55111a2
solidDoWant <fred.heinecke@yahoo.com>
2025-09-16 05:10:25 +0000

    block reserved CIDRs on the gateways
    Signed-off-by: solidDoWant <fred.heinecke@yahoo.com>

--- cluster/gitops/networking/vpn/gateway/scripts/setup-nat.sh
+++ cluster/gitops/networking/vpn/gateway/scripts/setup-nat.sh
@@ -27,4 +27,24 @@ done
 # Sending packets for with a source IP within the VPN gateway subnet would result in the destination not knowing how to route the reply packets back
 iptables -t nat -A POSTROUTING -o "${VPN_INTERFACE}" --match addrtype ! --dst-type LOCAL,BROADCAST,ANYCAST,MULTICAST -j MASQUERADE
 
-# TODO iptables rule to block traffic from tun0
+# Prevent packets with a destination IP within the blocked CIDRs from being sent out the VPN interface
+# This is in case something is misconfigured and tries to send a packet to one of these addresses via the default route
+CIDRS_TO_BLOCK="\
+0.0.0.0/8 \
+10.0.0.0/8 \
+100.64.0.0/10 \
+127.0.0.0/8 \
+169.254.0.0/16 \
+172.16.0.0/12 \
+192.0.0.0/24 \
+192.0.2.0/24 \
+192.88.99.0/24 \
+192.168.0.0/16 \
+198.18.0.0/15 \
+198.51.100.0/24 \
+203.0.113.0/24 \
+224.0.0.0/3"
+
+for CIDR_TO_BLOCK in ${CIDRS_TO_BLOCK}; do
+    iptables -t filter -A OUTPUT -o "${VPN_INTERFACE}" -d "${CIDR_TO_BLOCK}" -j REJECT
+done
